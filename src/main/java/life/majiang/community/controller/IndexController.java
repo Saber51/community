4b22cca00bc0940d6e7e371b82e0daea8d88cdff
saber <luanzhaofei@outlook.com>
2019-10-23 12:27:41 +0800

    添加google收录

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -44,6 +45,11 @@ public class IndexController {
         model.addAttribute("tagQuestions",hotTagCache.getTagQuestions());
         model.addAttribute("tagComments", hotTagCache.getTagComments());
         return "index";
+    }
+
+    @RequestMapping("/google1ccda0fc28d18c19.html")
+    public String googleAdnalytics(){
+        return "google1ccda0fc28d18c19";
     }
 
 }
